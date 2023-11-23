2bb056f85b768d269c50c54bcc956789f4c182fd
rubysteinmetz <150848668+rubysteinmetz@users.noreply.github.com>
2023-11-23 23:11:10 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
 console.log("hi");
+
+var openMenuButton = document.querySelector("header > button");
+var navigatie = document.querySelector("header nav");
+
+openMenuButton.onclick = openMenu;
+
+function openMenu() {
+  navigatie.classList.add("open");
+}
+
+var sluitMenuButton = document.querySelector("header nav button");
+
+sluitMenuButton.onclick = sluitMenu;
+
+function sluitMenu() {
+  navigatie.classList.remove("open");
+}
+
