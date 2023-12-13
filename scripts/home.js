92e620552e6af1edcd42d10a2ce963444a3a10b1
rubysteinmetz <150848668+rubysteinmetz@users.noreply.github.com>
2023-12-13 17:37:31 +0100

    Add files via upload

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,18 @@
 console.log("hooiiiii");
 
-var plattegrondButton = document.querySelector("section:nth-of-type(6) div > button");
-var plattegrondDialog = document.querySelector("section:nth-of-type(6) div > dialog");
+var plattegrondButton = document.querySelector("section:nth-of-type(3) div > button");
+var plattegrondDialog = document.querySelector("section:nth-of-type(3) div > dialog");
 
 plattegrondButton.onclick = openPlattegrond;
 
 function openPlattegrond() {
   plattegrondDialog.showModal();
 }
+
+
+
+
+
+
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ openMenuButton.onclick = openMenu;
 
 function openMenu() {
   navigatie.classList.add("open");
+  document.documentElement.classList.add("open");
 }
 
 var sluitMenuButton = document.querySelector("header nav button");
@@ -16,5 +17,38 @@ sluitMenuButton.onclick = sluitMenu;
 
 function sluitMenu() {
   navigatie.classList.remove("open");
+  document.documentElement.classList.remove("open");
+}
+
+var responsiveNav = document.querySelector("header nav section > h2:first-of-type");
+var eersteLijst = document.querySelector("header nav ul:first-of-type")
+
+responsiveNav.onmouseover = menuOpenklappen
+
+function menuOpenklappen() {
+  responsiveNav.classList.add("hallo");
+  document.elementFromPoint.classList.add("hallo");
+}
+
+
+window.onresize = checkDetails;
+
+const details = document.querySelectorAll("details");
+
+function checkDetails() {
+  const vpWidth = window.innerWidth;
+
+  if (vpWidth > 640) {
+    console.log("breed");
+
+    details.forEach(detail => {
+      detail.open = true;
+    });
+  }
+  else {
+    details.forEach(detail => {
+      detail.open = false;
+    });
+  }
 }
 
